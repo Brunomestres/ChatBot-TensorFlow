3294b830aba05f84f79fb66dcdf61caf2cf21234
unknown <brunomestres@gmail.com>
2021-07-14 12:47:57 -0300

    first commit

---
+++ chatbot.py
@@ -0,0 +1,163 @@
+import numpy as np
+import tensorflow as tf
+import re
+import time
+
+
+#Pré-processamento de dados
+#Importando a base de dados
+linhas = open('movie_lines.txt', encoding= 'utf-8', errors='ignore').read().split('\n')
+conversas = open('movie_conversations.txt', encoding= 'utf-8', errors='ignore').read().split('\n')
+
+#Criando o dicionario para mapear cada linha com seu ID
+
+id_para_linha = {}
+for linha in linhas:
+    _linha  = linha.split(' +++$+++ ')
+    if len(_linha) == 5:
+        id_para_linha[_linha[0]] = _linha[4]
+        
+conversas_id = [] 
+for conversa in conversas[:-1]:
+    _conversa = conversa.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
+    conversas_id.append(_conversa.split(','))
+    
+perguntas = []
+respostas = []
+for conversa in conversas_id:
+    for i in range(len(conversa) - 1):
+        perguntas.append(id_para_linha[conversa[i]])
+        respostas.append(id_para_linha[conversa[i + 1]])
+        
+        
+        
+def limpa_texto(texto):
+    texto = texto.lower()
+    texto = re.sub(r"i'm","i am", texto)
+    texto = re.sub(r"she's","she is", texto)
+    texto = re.sub(r"he's","he is", texto)
+    texto = re.sub(r"that's","that is", texto)
+    texto = re.sub(r"what's","what is", texto)
+    texto = re.sub(r"where's","where is", texto)
+    texto = re.sub(r"\'ll","will", texto)
+    texto = re.sub(r"\'ve","have", texto)
+    texto = re.sub(r"\'re","are", texto)
+    texto = re.sub(r"\'d","would", texto)
+    texto = re.sub(r"wont't","will not", texto)
+    texto = re.sub(r"can't","cannot", texto)
+    texto = re.sub(r"[-()#/@;:<>{}~+=?.|,]","", texto)
+    return texto
+    
+    
+perguntas_limpas = []
+for pergunta in perguntas:
+    perguntas_limpas.append(limpa_texto(pergunta))
+
+respostas_limpas = []
+for resposta in respostas:
+    respostas_limpas.append(limpa_texto(resposta))
+
+    
+
+palavras_contagem = {}
+for pergunta in perguntas_limpas:
+    for palavra in pergunta.split():
+        if palavra not in palavras_contagem:
+            palavras_contagem[palavra] = 1
+        else:
+            palavras_contagem[palavra] += 1
+
+for resposta in respostas_limpas:
+    for palavra in resposta.split():
+        if palavra not in palavras_contagem:
+            palavras_contagem[palavra] = 1
+        else:
+            palavras_contagem[palavra] += 1
+
+limite = 20
+perguntas_palavras_int = {}
+numero_palavra = 0
+for palavra, contagem in palavras_contagem.items():
+    if contagem >= limite:
+        perguntas_palavras_int[palavra] = numero_palavra
+        numero_palavra +=1
+
+respostas_palavras_int = {}
+numero_palavra = 0
+for palavra, contagem in palavras_contagem.items():
+    if contagem >= limite:
+        respostas_palavras_int[palavra] = numero_palavra
+        numero_palavra +=1
+        
+tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
+
+for token in tokens:
+    perguntas_palavras_int[token] = len(perguntas_palavras_int) + 1
+    
+for token in tokens:
+    respostas_palavras_int[token] = len(respostas_palavras_int) + 1
+
+respostas_int_palavras = {p_i: p for p, p_i in respostas_palavras_int.items() }
+
+
+
+
+for i in range(len(respostas_limpas)):
+    respostas_limpas[i] += ' <EOS>'
+
+
+
+perguntas_para_int = []
+for pergunta in perguntas_limpas:
+    ints = []
+    for palavra in pergunta.split():
+        if palavra not in perguntas_palavras_int:
+            ints.append(perguntas_palavras_int['<OUT>'])
+        else:
+            ints.append(perguntas_palavras_int[palavra])
+    perguntas_para_int.append(ints)
+        
+    
+    
+respostas_para_int = []
+for resposta in respostas_limpas:
+    ints = []
+    for palavra in resposta.split():
+        if palavra not in respostas_palavras_int:
+            ints.append(respostas_palavras_int['<OUT>'])
+        else:
+            ints.append(respostas_palavras_int[palavra])
+    respostas_para_int.append(ints)
+
+
+
+
+
+perguntas_limpas_ordenadas = []
+respostas_limpas_ordenadas = []
+for tamanho in range(1,25 + 1):
+    for i in enumerate(perguntas_para_int):
+        if len(i[1]) == tamanho:
+            perguntas_limpas_ordenadas.append(perguntas_para_int[i[0]])
+            respostas_limpas_ordenadas.append(respostas_para_int[i[0]])
+
+
+
+
+def entradas_modelos():
+    entradas = tf.placeholder(tf.int32, [None, None], name = 'entradas')
+    saidas = tf.placeholder(tf.int32, [None, None], name = 'saidas')
+    lr = tf.placeholder(tf.float32, name = 'learning_rate')
+    keep_prob = tf.placeholder(tf.float32, name = 'keep_prob')
+    return entradas, saidas, lr, keep_prob
+
+
+def preprocessamento_saida(saidas, palavra_para_int, batch_size):
+    esquerda = tf.fill([batch_size, 1], palavra_para_int['<SOS>'])
+    direita = tf.strided_slice(saidas, [0,0], [batch_size, -1], strides= [1,1])
+    saidas_preprocessadas = tf.concat([esquerda, direita], 1)
+    return saidas_preprocessadas
+
+
+def rnn_codificador(rnn_entradas, rnn_tamanho, numero_camadas, keep_prob, tamanho_sequencia):
+    
